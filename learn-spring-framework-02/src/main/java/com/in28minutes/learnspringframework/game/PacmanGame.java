ed03d16ad419979420707eceb3f80e34ee3c0727
Taehyoun Kim <kth9903@gmail.com>
2023-10-01 01:51:51 +0900

    add basic spring framework study file 2

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,23 @@
+package com.in28minutes.learnspringframework.game;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PacmanGame implements GamingConsole{
+
+		public void up() {
+			System.out.println("right");
+		}
+		
+		public void down() {
+			System.out.println("left");
+		}
+		
+		public void left() {
+			System.out.println("up");
+		}
+		
+		public void right() {
+			System.out.println("down");
+		}
+}
